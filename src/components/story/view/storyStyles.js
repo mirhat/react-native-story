ac5712dd3281f47b5daa54e536cd0df8e24f9768
Mirhat Babic <mirhat.babic@orbicode.com>
2020-03-29 23:15:44 +0200

    Fixed story margin top

--- src/components/story/view/storyStyles.js
+++ src/components/story/view/storyStyles.js
@@ -2,9 +2,7 @@ import { StyleSheet, Platform } from "react-native";
 import { Dimensions } from "react-native";
 
 export default StyleSheet.create({
-  storyListContainer: {
-    marginTop: 50
-  },
+  storyListContainer: {},
   modal: {
     height: Dimensions.get("window").height,
     width: Dimensions.get("window").width,
